e53ac5c9b4d41bf82f3db76b7c3136db93f195c4
Arielle Adams <arielle919@gmail.com>
2013-12-12 17:57:05 -0600

    notes and test 5 save

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -4,19 +4,23 @@ class LinkedListItem
   #instance variable '@my_variable'. Which saves you from defining these
   #methods yourself
   include Comparable
-  #The Comparable mixin is used by classes whose objects may be ordered. The class must define the <=> operator, which compares the receiver against another object, returning -1, 0, or +1 depending on whether the receiver is less than, equal to, or greater than the other object. If the other object is not comparable then the <=> operator should return nil. Comparable uses <=> to implement the conventional comparison operators (<, <=, ==, >=, and >) and the method between?.
+  #The Comparable mixin is used by classes whose objects may be ordered.
+  #The class must define the <=> operator, which compares the receiver
+  #against another object, returning -1, 0, or +1 depending on whether
+  #the receiver is less than, equal to, or greater than the other object.
+  #If the other object is not comparable then the <=> operator should return nil.
+  #Comparable uses <=> to implement the conventional comparison operators
+  #(<, <=, ==, >=, and >) and the method between?.
   #http://ruby-doc.org/core-2.0.0/Comparable.html
 
   def initialize(payload)
     @payload = "foo"
-
   end
 
-  def test_for_raise_NoMethod_Error #AA
-      assert_raise(NoMethodError) do
-        LinkedListItem.new
-        llil.next_list_item = "llil1"
-      end
+  def test_03_cant_set_self_as_next_list_item #AA
+    assert_raise (ArgumentError) do
+      next_list_item.lli1
+    end
   end
 
 end
