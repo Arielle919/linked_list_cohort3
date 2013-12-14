7a1cec95258df0028ded0778a865a981797aff72
Arielle Adams <arielle919@gmail.com>
2013-12-14 16:22:08 -0600

    test1-13CODE:refactored

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,43 +1,39 @@
 class LinkedListItem
   attr_accessor :payload, :next_list_item
-  #attr_accessor :my_variable creates the methods to assign and to read the
-  #instance variable '@my_variable'. Which saves you from defining these
-  #methods yourself
   include Comparable
-  #The Comparable mixin is used by classes whose objects may be ordered.
-  #The class must define the <=> operator, which compares the receiver
-  #against another object, returning -1, 0, or +1 depending on whether
-  #the receiver is less than, equal to, or greater than the other object.
-  #If the other object is not comparable then the <=> operator should return nil.
-  #Comparable uses <=> to implement the conventional comparison operators
-  #(<, <=, ==, >=, and >) and the method between?.
 
-  #everything with def is a method for the class LinkedListItem
   def initialize(payload)
     @payload = payload
   end
 
+ #everything with def is a method for the class LinkedListItem
   def next_list_item=(next_list_item) #initializes this method to be used
     if next_list_item == self#self in instance of this class http://www.jimmycuadra.com/posts/self-in-ruby
       raise ArgumentError, "Self can't be set as following item"
     else
       @next_list_item = next_list_item
     end
+  end
 
-    def last? #the last item is nil
-      @next_list_item.nil?
+  def last? #the last item is nil
+    self.next_list_item.nil?
+  end
+
+   def === linked_list_item
+    if self.object_id == linked_list_item.object_id
+      true
     end
+  end
 
-    def <=> (second_list_item) #using comparable here
-      return 1 if self.payload.is_a?(Symbol) && second_list_item.payload.is_a?(String)
-      return -1 if self.payload.is_a?(String) && second_list_item.payload.is_a?(Symbol)#assert "bar" < :apple
-      #!!Important: self.payload is the reciever and it is less than the
-      #object being compared to it = -1 , the other way around = 1 :)
-      return -1 if self.payload.is_a?(Fixnum) && second_list_item.payload.is_a?(String) #1 < string "bar" Number is less than string
-      return 1 if self.payload.is_a?(String) && second_list_item.payload.is_a?(Fixnum) #"bar" > 1
-
-      self.payload <=> second_list_item.payload
-    end
+  def <=> compared_linked_list_item
+    #objects being compared :apple > "bar"
+    return 1 if self.payload.is_a?(Symbol) and compared_linked_list_item.payload.is_a?(String)
+    return -1 if self.payload.is_a?(String) and compared_linked_list_item.payload.is_a?(Symbol)
+    return -1 if self.payload.is_a?(Fixnum) and compared_linked_list_item.payload.is_a?(String)
+    return 1 if self.payload.is_a?(String) and compared_linked_list_item.payload.is_a?(Fixnum)
+    return -1 if self.payload.is_a?(Fixnum) and compared_linked_list_item.payload.is_a?(Symbol)
+    return 1 if self.payload.is_a?(Symbol) and compared_linked_list_item.payload.is_a?(Fixnum)
+    self.payload <=> compared_linked_list_item.payload
   end
 
 end
