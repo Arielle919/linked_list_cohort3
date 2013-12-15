6c16d91801fde0eb885cc768f1cffb34fae25ada
Arielle Adams <arielle919@gmail.com>
2013-12-15 13:46:31 -0600

    20 test passed

---
+++ lib/linked_list.rb
@@ -0,0 +1,41 @@
+class LinkedList
+  attr_accessor :payload, :head_item
+
+  def initialize(*payload)
+    @count = 0
+    @last_item = nil
+  end
+
+  def add_item payload
+    if @head_item == nil
+      @head_item = LinkedListItem.new(payload)
+      @count += 1
+      initial_node = @head_item
+    else
+      initial_node = @head_item
+      while initial_node.next_list_item != nil
+        initial_node = initial_node.next_list_item
+      end
+      initial_node.next_list_item = LinkedListItem.new(payload)
+      @count += 1
+      @last_item = initial_node.next_list_item
+    end
+  end
+
+  def get(index)
+    if index < 0
+      raise IndexError
+    elsif @head_item == nil
+      raise IndexError
+    else
+      initial_node = @head_item
+      index.times do #this is a ruby loop
+        initial_node == nil ? (raise IndexError) : initial_node = initial_node.next_list_item
+      end
+      initial_node.payload
+    end
+  end
+
+
+
+end
